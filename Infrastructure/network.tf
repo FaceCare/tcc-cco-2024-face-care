942d10522fd54e579911f4982c68148aac6ef7e2
Rodrigo Garcez Hermann <rodrigo.hermann@yhub.com.br>
2024-04-13 15:20:14 -0300

    [DEVOPS] feat: add nat gateway

--- Infrastructure/network.tf
+++ Infrastructure/network.tf
@@ -15,6 +15,21 @@ resource "aws_internet_gateway" "ig" {
     Name = "${local.project_name}-igw"
   }
 }
+
+/* Elastic IP for NAT */
+resource "aws_eip" "nat_eip" {
+  depends_on = [aws_internet_gateway.ig]
+}
+/* NAT */
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
+  depends_on    = [aws_internet_gateway.ig]
+  tags = {
+    Name = "${local.project_name}-nat"
+  }
+}
+
 /* Public subnet */
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
@@ -56,6 +71,12 @@ resource "aws_route" "public_internet_gateway" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.ig.id
 }
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = "${aws_route_table.private.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+}
 /* Route table associations */
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnets_cidr)
@@ -66,4 +87,25 @@ resource "aws_route_table_association" "private" {
   count          = length(var.private_subnets_cidr)
   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private.id
+}
+
+/*==== VPC's Default Security Group ======*/
+resource "aws_security_group" "default" {
+  name        = "${local.project_name}-default-sg"
+  description = "Default security group to allow inbound/outbound from the VPC"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = true
+  }
+  
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = "true"
+  }
 }
