9ff19b4cb5b464e4d45cfafea3f45b1c56020b73
EnanHenrique <enan.oliveira@bandtec.com.br>
2024-04-21 18:39:25 -0300

    reorganize files site backend and finish api first version

--- site/backend/api/config/database.py
+++ site/backend/api/config/database.py
@@ -1,10 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 from os import getenv
-
-load_dotenv() # TODO: remove this when add main.py in the root of project
 
 USER_DB = getenv('USER_DB')
 PASSWD_DB = getenv('PASSWD_DB')
