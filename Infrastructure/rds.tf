3fdc342044e382eb1036d2ccd8760bb24a924abc
Rodrigo Garcez Hermann <rodrigo.hermann@yhub.com.br>
2024-05-06 09:03:37 -0300

    [DEVOPS] chore: removendo rds pois ficara na ec2

--- Infrastructure/rds.tf
+++ Infrastructure/rds.tf
@@ -1,26 +1,26 @@
-resource "aws_db_subnet_group" "public_subnet_group" {
-  name       = "main"
-  subnet_ids = [aws_subnet.public_subnet[0].id,aws_subnet.public_subnet[1].id]
+# resource "aws_db_subnet_group" "public_subnet_group" {       # vamos utilizar o banco na propria ec2
+#   name       = "main" 
+#   subnet_ids = [aws_subnet.public_subnet[0].id,aws_subnet.public_subnet[1].id]
 
-  tags = {
-    Name = "${local.project_name}_subnet_group"
-  }
-}
+#   tags = {
+#     Name = "${local.project_name}_subnet_group"
+#   }
+# }
 
-resource "aws_db_instance" "rds_instance" {
-  engine                 = "mysql"
-  db_name                = "tcc_2024"
-  identifier             = "${local.project_name}-rds"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 10
-  publicly_accessible    = true
-  username               = var.db_username
-  password               = var.db_password
-  vpc_security_group_ids = [aws_security_group.allow_rds.id]
-  db_subnet_group_name   = aws_db_subnet_group.public_subnet_group.id
-  skip_final_snapshot    = true
+# resource "aws_db_instance" "rds_instance" {        # vamos utilizar o banco na propria ec2
+#   engine                 = "mysql"
+#   db_name                = "tcc_2024"
+#   identifier             = "${local.project_name}-rds"
+#   instance_class         = "db.t3.micro"
+#   allocated_storage      = 10
+#   publicly_accessible    = true
+#   username               = var.db_username
+#   password               = var.db_password
+#   vpc_security_group_ids = [aws_security_group.allow_rds.id]
+#   db_subnet_group_name   = aws_db_subnet_group.public_subnet_group.id
+#   skip_final_snapshot    = true
 
-  tags = {
-    Name = "${local.project_name}-rds"
-  }
-}
+#   tags = {
+#     Name = "${local.project_name}-rds"
+#   }
+# }
--- Infrastructure/security.tf
+++ Infrastructure/security.tf
@@ -23,29 +23,29 @@ resource "aws_security_group" "allow_jupiter" {
   }
 }
 
-resource "aws_security_group" "allow_rds" {
-  name        = "${local.project_name}-rds-sg"
-  description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.vpc.id
+# resource "aws_security_group" "allow_rds" {  # vamos utilizar o banco na propria ec2
+#   name        = "${local.project_name}-rds-sg"
+#   description = "Allow inbound traffic"
+#   vpc_id      = aws_vpc.vpc.id
 
-  ingress {
-    from_port   = 0
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 0
+#     to_port     = 3306
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "${local.project_name}-rds-sg"
-  }
-}
+#   tags = {
+#     Name = "${local.project_name}-rds-sg"
+#   }
+# }
 
 resource "aws_security_group" "lambda_security_group" {
   name        = "${local.project_name}-lambda-sg"
