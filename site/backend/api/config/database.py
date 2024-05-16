14f2e0c1651963dd399cdf4f41980827a356ae14
EnanHenrique <enan.oliveira@bandtec.com.br>
2024-05-16 20:23:40 -0300

    change database url

--- site/backend/api/config/database.py
+++ site/backend/api/config/database.py
@@ -3,10 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from os import getenv
 
-USER_DB = getenv('USER_DB')
-PASSWD_DB = getenv('PASSWD_DB')
-HOST_BD = getenv('HOST_BD')
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USER_DB}:{PASSWD_DB}@{HOST_BD}:3306/tcc"
+SQLALCHEMY_DATABASE_URL = getenv('DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, 
