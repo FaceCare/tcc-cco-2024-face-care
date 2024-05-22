d8621d362ccd525160dacf3ad2f42ce1f162927b
EnanHenrique <enan.oliveira@bandtec.com.br>
2024-05-22 20:45:37 -0300

    v1 model working

--- machine_learning/model/model_train_v1.py
+++ machine_learning/model/model_train_v1.py
@@ -6,13 +6,12 @@ from skimage.feature import hog
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from skimage.color import rgb2gray
 import boto3
 import shutil
 import joblib
 from datetime import datetime
+import pathlib
 
-# boto3.setup_default_session(profile_name="faculdade") # TODO: remove in production
 s3 = boto3.client('s3')
 bucket_staged = 'tcc-dev-staged-bucket'
 bucket_model = 'tcc-dev-saved-model-bucket'
@@ -25,36 +24,34 @@ for page in pages:
     for obj in page['Contents']:
         files_s3.append(obj['Key'])
 
-files_acne = [f for f in files_s3 if str(f).startswith('Acne/')]
-files_no_acne = [f for f in files_s3 if str(f).startswith('SemAcne/')]
-labels = ['Acne', 'SemAcne']
+files_acne = [f for f in files_s3 if str(f).startswith('Acne/') and f != 'Acne/']
+files_no_acne = [f for f in files_s3 if str(f).startswith('SemAcne/') and f != 'SemAcne/']
+folders_imgs = ['Acne', 'SemAcne']
 
 def download_list_files_s3(files: list, bucket_name: str, save_dir=''):
+    save_dir = os.path.join('svm', save_dir)
     if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
+        pathlib.Path(save_dir).mkdir(parents=True, exist_ok=True)
         
-    count=0
     for f in files:
         print(f'Downloading file={f}')
         s3.download_file(bucket_name, f, os.path.join(save_dir, os.path.split(f)[-1]))
-        count+=1
-        if count==10:
-            break
 
-download_list_files_s3(files_acne, bucket_staged, labels[0])
-download_list_files_s3(files_no_acne, bucket_staged, labels[1])
+download_list_files_s3(files_acne, bucket_staged, folders_imgs[0])
+download_list_files_s3(files_no_acne, bucket_staged, folders_imgs[1])
 
 def load_data(data_dir, img_size=(128, 128)):
+    labels = []
     features = []
-    for label in labels:
+    for label in ['Acne', 'SemAcne']:
         folder = os.path.join(data_dir, label)
         for file in os.listdir(folder):
             img_path = os.path.join(folder, file)
             img = imread(img_path)
             img_resized = resize(img, img_size)
-            hog_features = hog(img_resized, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, channel_axis=-1)
+            hog_features = hog(img_resized, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False)
             features.append(hog_features)
-            labels.append(0 if label == labels[1] else 1)
+            labels.append(0 if label == 'SemAcne' else 1)
     return np.array(features), np.array(labels)
 
 # Carregar dados
@@ -65,6 +62,7 @@ X, y = load_data(data_dir)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, train_size=0.2)
 
 # Treinar um classificador SVM
+print('Training model...')
 clf = SVC(kernel='linear')
 clf.fit(X_train, y_train)
 
@@ -76,11 +74,12 @@ print(f'Accuracy: {accuracy * 100:.2f}%')
 # Salvar o modelo
 model_file = f"{datetime.now().strftime('%d-%m-%Y %H-%M-%S.%f')} svm_model.pkl"
 joblib.dump(clf, model_file)
-print(f'Modelo salvo em {model_file}')
+print(f'Model saved {model_file}')
 
 # Upando novo modelo treinado para bucket s3
+print(f'Uploading model to s3: {model_file}')
 s3.upload_file(model_file, bucket_model, model_file)
 
-for label in labels:
-    shutil.rmtree(os.path.join(data_dir, label))
+print('Cleaning files model')
+shutil.rmtree(data_dir)
 os.remove(model_file)
