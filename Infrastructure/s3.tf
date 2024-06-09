ba1df0e2d0cf1df66cb2b1f229f512ffb4fb8de4
rodrigo garcez <rodrigo.hermann@sptech.school>
2024-06-09 15:24:45 -0300

    [DEVOPS] chore: removendo bucket staged

--- Infrastructure/s3.tf
+++ Infrastructure/s3.tf
@@ -5,15 +5,6 @@ resource "aws_s3_bucket" "s3_raw" {
     Name = "${local.project_name}-raw-bucket"
   }
 }
-
-resource "aws_s3_bucket" "s3_staged" {
-  bucket = "${local.project_name}-staged-bucket"
-
-  tags = {
-    Name = "${local.project_name}-staged-bucket"
-  }
-}
-
 resource "aws_s3_bucket" "s3_consumed" {
   bucket = "${local.project_name}-consumed-bucket"
 
@@ -21,15 +12,6 @@ resource "aws_s3_bucket" "s3_consumed" {
     Name = "${local.project_name}-consumed-bucket"
   }
 }
-
-# resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
-#   bucket = aws_s3_bucket.s3_raw.id
-#   lambda_function {
-#     lambda_function_arn = aws_lambda_function.image_processing.arn
-#     events              = ["s3:ObjectCreated:*"]
-#   }
-# }
-
 
 resource "aws_s3_bucket" "s3_saved_model" {
   bucket = "${local.project_name}-saved-model-bucket"
