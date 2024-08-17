06feacf117b3af5b43e7c79b5f45284e61852931
rodrigo garcez <rodrigo.hermann@sptech.school>
2024-08-17 16:13:12 -0300

    [DEVOPS] chore: refactor tratamento img

--- tratamentoImagem/src/app.py
+++ tratamentoImagem/src/app.py
@@ -3,45 +3,31 @@ import cv2
 import os
 import numpy as np
 from botocore.exceptions import NoCredentialsError
-from sklearn.decomposition import PCA
-
 
 def download_image(bucket_name, image_key, local_filename):
     s3 = boto3.client('s3')
     try:
         s3.download_file(bucket_name, image_key, local_filename)
+        return True
     except NoCredentialsError:
         print("Credenciais não configuradas corretamente ou não disponíveis")
         return False
-    return True
 
-def load_image_grayscale(filename):
-    image = cv2.imread(filename)
-    if image is None:
-        print(f"Erro ao carregar a imagem {filename}")
-        return None
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return grayscale_image
+def detect_and_cover_eyes(image):
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-
-def apply_pca(image, variance_ratio=0.9):
+    eyes = eye_cascade.detectMultiScale(image, scaleFactor=1.07, minNeighbors=35, minSize=(50, 30))
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0), -1)
     
-    flat_image = image.flatten().astype(np.float32)
-    
-    pca = PCA(n_components=variance_ratio, svd_solver='full')
-    transformed = pca.fit_transform(flat_image.reshape(1, -1))
-    reconstructed = pca.inverse_transform(transformed)
-    pca_image = reconstructed.reshape(image.shape)
-    return pca_image
+    return image
 
+def convert_to_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def resize_image(image, scale_percent=50):
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-    return resized
-
+def resize_image(image, output_size):
+    resized_image = cv2.resize(image, output_size, interpolation=cv2.INTER_AREA)
+    return resized_image
 
 def upload_image(bucket_name, image_key, local_filename):
     s3 = boto3.client('s3')
@@ -50,41 +36,29 @@ def upload_image(bucket_name, image_key, local_filename):
     except NoCredentialsError:
         print("Credenciais não configuradas corretamente ou não disponíveis")
 
+def process_image(image_key, local_filename, bucket_staged):
+    if download_image(bucket_raw, image_key, local_filename):
+        image = cv2.imread(local_filename)
+        if image is not None:
+            #processed_image = detect_and_cover_eyes(image)  # Detecta e cobre os olhos na imagem colorida
+            grayscale_image = convert_to_grayscale(image)  # Converte a imagem processada em escala de cinza
+            resized_image = resize_image(grayscale_image, (512, 512))  # Redimensiona a imagem
+            cv2.imwrite(local_filename, resized_image)
+            upload_image(bucket_staged, image_key, local_filename)
+            print(f"Imagem {image_key} processada e salva no bucket staged")
+        os.remove(local_filename)
 
 env = os.getenv('ENVIRONMENT', 'DEV')  # Por padrão, considere que não é PROD
 
-if env != 'PROD':
-    boto3.setup_default_session(profile_name="faculdade")
+boto3.setup_default_session(profile_name="faculdade")
 
 bucket_raw = 'tcc-dev-raw-bucket'
-bucket_staged = 'tcc-dev-consumed-bucket'
+bucket_consumed = 'tcc-dev-consumed-bucket'
 
 s3 = boto3.resource('s3')
-
-
-raw_bucket = s3.Bucket(bucket_raw)
-raw_images = [obj.key for obj in raw_bucket.objects.all()]
-
-
-staged_bucket = s3.Bucket(bucket_staged)
-staged_images = [obj.key for obj in staged_bucket.objects.all()]
-
+raw_images = [obj.key for obj in s3.Bucket(bucket_raw).objects.all()]
+staged_images = {obj.key for obj in s3.Bucket(bucket_consumed).objects.all()}
 
 for image_key in raw_images:
     if image_key not in staged_images:
-        local_filename = os.path.join('temp_image.jpg')
-        
-        if download_image(bucket_raw, image_key, local_filename):
-            
-            grayscale_image = load_image_grayscale(local_filename)
-            if grayscale_image is not None:
-                
-                resized_image = resize_image(grayscale_image)
-                
-                pca_image = apply_pca(resized_image)
-                
-                processed_image_key = image_key  
-                cv2.imwrite(local_filename, pca_image.astype(np.uint8))  
-                upload_image(bucket_staged, processed_image_key, local_filename)
-                print(f"Imagem {image_key} processada e salva no bucket staged")
-            os.remove(local_filename)
+        process_image(image_key, 'temp_image.jpg', bucket_consumed)
