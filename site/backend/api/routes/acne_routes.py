4fc8dd498ad4e456c061a873d992f9ce142fd687
EnanHenrique <enan.oliveira@bandtec.com.br>
2024-10-06 15:48:37 -0300

    [dev]: add garbage collector to acne  get report

--- site/backend/api/routes/acne_routes.py
+++ site/backend/api/routes/acne_routes.py
@@ -1,18 +1,21 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import Response
 from fastapi import UploadFile
+import  gc
 
 from service.photo_service import PhotoService
 from service.acne_service import AcneService
 
 app_router = APIRouter()
-acne_service = AcneService()
 
 @app_router.post('/report')
 def post(
     request: Request,
     photo: UploadFile = Depends(PhotoService.validate_photo)
 ):
+    acne_service = AcneService()
     last_model_keras_name = request.app.state.model_keras
     report = acne_service.get_acne_report(photo, last_model_keras_name)
+    del acne_service
+    gc.collect()
     return Response(str(report), 200)
